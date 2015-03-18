c5ba796deecb1810a6a91b63bcf96cd243c63a76
Dieter Niklaus <dieter.niklaus@gmx.net>
2015-03-18 15:16:43 +0100

    DbgTrace_Port implements Arduino Print, output channel: Arduino Hardware Serial (hardcoded); first trial, compiles but is untested yet :-)
    Signed-off-by: Dieter Niklaus <dieter.niklaus@gmx.net>

--- debug_trace/DbgTracePort.cpp
+++ debug_trace/DbgTracePort.cpp
@@ -6,9 +6,17 @@
  */
 
 #include <DbgTracePort.h>
+#include <string.h>
+#include <Arduino.h>
 
-DbgTrace_Port::DbgTrace_Port()
+DbgTrace_Port::DbgTrace_Port(const char* tag)
 : m_level(DbgTrace_Port::notice)
+, m_tag(tag)
+{ }
+
+DbgTrace_Port::DbgTrace_Port(const __FlashStringHelper* tag)
+: m_level(DbgTrace_Port::notice)
+, m_tag(reinterpret_cast<const char*>(tag))
 { }
 
 DbgTrace_Port::~DbgTrace_Port()
@@ -22,4 +30,30 @@ void DbgTrace_Port::setLevel(DbgTraceLevel level)
 DbgTrace_Port::DbgTraceLevel DbgTrace_Port::getLevel()
 {
   return m_level;
+}
+
+const char* DbgTrace_Port::getTag()
+{
+  return m_tag;
+}
+
+inline size_t DbgTrace_Port::write(const uint8_t *buffer, size_t size)
+{
+  size_t n = 0;
+  n += Serial.print(millis());
+  n += Serial.print(F(" "));
+  n += Serial.print(getTag());
+  while (size--)
+  {
+    n += Serial.write(*buffer++);
+  }
+  n += Serial.println();
+  return n;
+}
+
+inline size_t DbgTrace_Port::write(uint8_t num)
+{
+  size_t n = 0;
+  n += Serial.write(num);
+  return n;
 }
--- debug_trace/DbgTracePort.h
+++ debug_trace/DbgTracePort.h
@@ -8,49 +8,49 @@
 #ifndef PLAT_DEBUG_TRACE_DBGTRACEPORT_H_
 #define PLAT_DEBUG_TRACE_DBGTRACEPORT_H_
 
-typedef enum
-{
-  none      =  -1,
-  emergency =   0,
-  alert     =   1,
-  critical  =   2,
-  error     =   3,
-  warning   =   4,
-  notice    =   5,
-  info      =   6,
-  debug     =   7
-} DbgTraceLevel;
+#include "Print.h"
 
-class DbgTrace_Port
+
+#define TR_PRINT(PORT, LEVEL, MSG)  do { if (((PORT)->getLevel()>=(LEVEL))) (PORT)->print((MSG)); } while (0);
+
+
+class DbgTrace_Port : public Print
 {
 public:
   typedef enum
   {
-    none      =  -1,
-    emergency =   0,
-    alert     =   1,
-    critical  =   2,
-    error     =   3,
-    warning   =   4,
-    notice    =   5,
-    info      =   6,
-    debug     =   7
+    none      = -1,
+    emergency =  0,
+    alert     =  1,
+    critical  =  2,
+    error     =  3,
+    warning   =  4,
+    notice    =  5,
+    info      =  6,
+    debug     =  7
   } DbgTraceLevel;
 
-  DbgTrace_Port();
+  DbgTrace_Port(const char* tag);
+  DbgTrace_Port(const __FlashStringHelper* tag);
   virtual ~DbgTrace_Port();
 
   void setLevel(DbgTraceLevel level);
 
   DbgTraceLevel getLevel();
 
+  const char* getTag();
+
+  virtual size_t write(const uint8_t *buffer, size_t size);
+  virtual size_t write(uint8_t num);
+
+
 private:
   DbgTraceLevel m_level;
+  const char* m_tag;
 
 private: // forbidden default functions
   DbgTrace_Port& operator = (const DbgTrace_Port& src); // assignment operator
   DbgTrace_Port(const DbgTrace_Port& src);              // copy constructor
-
 };
 
 #endif /* PLAT_DEBUG_TRACE_DBGTRACEPORT_H_ */
